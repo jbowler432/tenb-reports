def6d5cee6db3e551e772e8789ae5397c333e561
jbowler432 <jbowler432@gmail.com>
2022-03-08 18:17:08 +1000

    updates

--- beautifyResults.py
+++ beautifyResults.py
@@ -20,15 +20,27 @@ def compliance_result_summary(input_file):
 		results.append(data_subset)
 		#print(data_subset)
 	myTable=pd.DataFrame(results)
-	print(myTable)
+	#print(myTable)
 	grouped=myTable.groupby(['asset_uuid','audit_file','status'])
-	print(grouped.count())
+	#print(grouped.count())
 	grouped_counts=grouped.count().values
 	#print(grouped_counts)
 	counter=0
+	new_uuid=""
+	old_uuid=""
+	asset_dct={}
 	for (asset,audit,status), group in grouped:
-		print(str(counter),asset,audit,status,grouped_counts[counter][0])
+		new_uuid=asset
+		if new_uuid!=old_uuid:
+			asset_dct.update({asset:{'audit':audit,'failed':0,'passed':0,'warning':0,'error':0}})
+			asset_dct[asset].update({status.lower():grouped_counts[counter][0]})
+		else:
+			asset_dct[asset].update({status.lower():grouped_counts[counter][0]})
+		old_uuid=asset
+		#print(str(counter),asset,audit,status,grouped_counts[counter][0])
 		counter+=1
+	for (k,v) in asset_dct.items():
+		print(k,"\n",v,"\n")
 
 def vuln_result_summary(input_file):
 	decoded=read_json_file("../reports/all_vulns.json")
@@ -39,12 +51,45 @@ def vuln_result_summary(input_file):
 		results.append({'hostname':x['asset']['hostname'],'plugin':x['plugin']['description'],'severity':x['severity']})
 		#print(data_subset)
 	myTable=pd.DataFrame(results)
-	print(myTable)
+	#print(myTable)
 	grouped=myTable.groupby(['hostname','severity'])
-	print(grouped.count())
+	#print(grouped.count())
 	grouped_counts=grouped.count().values
 	#print(grouped_counts)
 	counter=0
+	host_old=""
+	host_new=""
+	host_dct={}
 	for (hostname,severity), group in grouped:
-		print(str(counter),hostname,severity,grouped_counts[counter][0])
+		host_new=hostname
+		if host_new != host_old:
+			host_dct.update({hostname:{'critical':0,'high':0,'medium':0,'low':0,'info':0}})
+			host_dct[hostname].update({severity:grouped_counts[counter][0]})
+		else:
+			host_dct[hostname].update({severity:grouped_counts[counter][0]})
+		#print(str(counter),hostname,severity,grouped_counts[counter][0])
 		counter+=1
+		host_old=hostname
+	for (k,v) in host_dct.items():
+		print(k,' - ',v)
+	table_str=dct_to_table(host_dct)
+	gen_html_report(table_str,"../reports/test.html")
+
+def dct_to_table(dct):
+	table_str="<table>"
+	for (k,v) in dct.items():
+		table_str=table_str+"<tr><td>"+k+str(v)+"</td>"
+	table_str=table_str+"</table>"
+	return table_str
+
+def gen_html_report(body,output_file):
+	fin=open('html_header.txt','r')
+	file_text=fin.read()
+	fin.close
+	fout=open(output_file,'w+')
+	fout.write(file_text)
+	fout.write("\n<body>")
+	fout.write('<div id="reportContent">\n')
+	fout.write(body)
+	fout.write('</div></html>')
+	fout.close()
--- runTests.py
+++ runTests.py
@@ -3,14 +3,14 @@ import tenbAbstract as ta
 import beautifyResults as br
 
 # extract some compliance data
-asset_lst=[]
-last_seen="01/09/2021"
-ta.asset_compliance_export_json("../io_keys.json","ep",asset_lst,last_seen,"../reports/compliance.json")
-br.compliance_result_summary("../reports/compliance.json")
+#asset_lst=[]
+#last_seen="01/07/2021"
+#ta.asset_compliance_export_json("../io_keys.json","sandbox",asset_lst,last_seen,"../reports/compliance.json")
+#br.compliance_result_summary("../reports/compliance.json")
 
 #cidr="0.0.0.0/0"
 #ta.cidr_vuln_export_json("../io_keys.json","sandbox",cidr,"../reports/all_vulns.json")
-#br.vuln_result_summary("../reports/all_vulns.json")
+br.vuln_result_summary("../reports/all_vulns.json")
 
-
+#ta.ip_vuln_report_html("../io_keys.json","sandbox","10.200.0.0/24","../reports/vulns.html")
 
