a05443d69df6705b2d7ad51725c4fbd9f9005be7
jbowler432 <jbowler432@gmail.com>
2022-09-19 13:10:46 +1000

    updates

--- runTests.py
+++ runTests.py
@@ -1,6 +1,7 @@
 import tenbIOcore as tc
 import tenbSCcore as sc
-import beautifyResults as br
+import htmlRoutines as hr
+import reportTemplates as rt
 import datetime
 import time
 import json
@@ -8,11 +9,29 @@ import json
 
 # file and directory locations
 key_file="../io_keys.json" # location of your key file
-sc_key_file="../sc_keys.json"
+#sc_key_file="../sc_keys.json"
 results_dir="results/" # the directory for your results
+reports_dir="report_samples/" # the directory for your results
 styles_dir="styles/" #style sheet location for web pages
-output_file=results_dir+"statistics_by_tag.html"
-results_file=results_dir+"vulns.json"
+html_file=reports_dir+"runtests.html"
+json_file=results_dir+"runtests_vulns.json"
 
 api_keys=tc.read_keys(key_file,"sandbox")
-sc_keys=sc.read_SC_keys(sc_key_file)
+#sc_keys=sc.read_SC_keys(sc_key_file)
+
+get_new_data=0
+
+if get_new_data==1:
+	# export some vuln data
+	num_assets=50
+	filters={"cidr_range": "192.168.16.0/24"}
+	payload={
+		"filters": filters,
+		"num_assets": num_assets
+	}
+	chunk_results=tc.check_and_download_vuln_chunks(api_keys,payload,json_file)
+
+'''
+produce html report off downloaded data
+'''
+rt.vulns_summary(json_file,html_file,styles_dir)
